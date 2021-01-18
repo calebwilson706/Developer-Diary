c5d16265b86265ce92cd23a8bad44cb5a0daebfa
Caleb Wilson <calebwilson706@gmail.com>
2021-01-18 18:33:22 +0000

    Initial Commit

---
+++ Crime Solver/Crime_SolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crime_SolverApp.swift
+//  Crime Solver
+//
+//  Created by Caleb Wilson on 18/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct Crime_SolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
